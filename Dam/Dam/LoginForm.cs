4d3f613f206393cbb5181eb169ab1793e49c85ba
Remmie Peyroux-Tangaroa <2021223@techtorium.ac.nz>
2018-11-22 10:51:28 +1300

    Login Form
    Created a login form that can login and navigate the user to their appropriate form (user will have user main form, admin will have admin main form)

---
+++ Dam/Dam/LoginForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Dam
+{
+    public partial class LoginForm : Form
+    {
+        public LoginForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            using (DB db = new DB())
+            {
+                List<Admin> adminUser = db.Admins.ToList();
+                foreach (Admin user in adminUser)
+                {
+                    if (tbUsername.Text == user.AdminName &&
+                        tbPassword.Text == user.AdminPassword)
+                    {
+                        MessageBox.Show("Logged In!", Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        if (user.Position == "Admin")
+                        {
+                            AdminForm mainForm = new AdminForm();
+                            mainForm.Show();
+                            Hide();
+                        }
+                        else
+                        {
+                            NormalForm mainForm = new NormalForm();
+                            mainForm.Show();
+                            Hide();
+                        }
+                        return;
+                    }
+                }
+                MessageBox.Show("Invalid Login Details!", Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
