fa2eb2a417edc1d5aae1299b6cea0f9f972751b4
Lew Paku-Tupou <2021202@techtorium.ac.nz>
2018-12-10 23:17:04 +1300

    Backup path
    changed the backup to the current folder

--- Dam/Dam/AdminForm.cs
+++ Dam/Dam/AdminForm.cs
@@ -79,7 +79,7 @@ namespace Dam
                 List<Metadata> bMetadatas = db.Metadatas.ToList();
                 List<Admin> bAdmins = db.Admins.ToList();
 
-                string backupPath = "\\\\DESKTOP-MK7928K\\TeamToo\\Backups\\Manual\\";
+                string backupPath = "\\\\DESKTOP-E2QDCCA\\TeamToo\\Backups\\Manual\\";
                 string path = backupPath + DateTime.Now.Year.ToString() + "-" + DateTime.Now.ToString("ddhhmmss") + ".txt";
                 File.Create(path).Close();
                 TextWriter tw = new StreamWriter(path, true);
